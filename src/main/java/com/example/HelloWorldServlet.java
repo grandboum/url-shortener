25634559a315ad356a89b075af150256a1a902f7
Illia Akhaiev <26223450+grandboum@users.noreply.github.com>
2023-01-14 13:53:16 +0000

    Working example of js making request to java backend

--- src/main/java/com/example/HelloWorldServlet.java
+++ src/main/java/com/example/HelloWorldServlet.java
@@ -1,18 +1,26 @@
 package com.example;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-@WebServlet(urlPatterns = {"/*"}, loadOnStartup = 1)
+@WebServlet(urlPatterns = {"/hello"}, loadOnStartup = 1)
 public class HelloWorldServlet extends HttpServlet
 {
- @Override
- public void doGet(HttpServletRequest request, HttpServletResponse response)
- throws IOException
- {
- response.getOutputStream().print("Hello World");
- }
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        Map<String, String> json = new HashMap<>();
+        json.put("text", "Hello, man");
+        Gson gson = new Gson();
+        
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println(gson.toJson(json));
+    }
 }
---
+++ src/main/webapp/main.js
@@ -0,0 +1,13 @@
+function showGreeting(greeting) {
+  var fieldNameElement = document.getElementById('field_name');
+  fieldNameElement.textContent = greeting;
+}
+
+async function fetchData(url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data["text"];
+}
+
+fetchData("/hello")
+    .then(showGreeting)
